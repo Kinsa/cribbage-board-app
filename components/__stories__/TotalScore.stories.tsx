7c502ff8c8e2e3a27721f7ac23f5c6c8491804df
kinsa <studio@kinsa.cc>
2025-12-05 14:19:59 +0000

    Adds stories for TotalScore and TurnScore

---
+++ components/__stories__/TotalScore.stories.tsx
@@ -0,0 +1,20 @@
+import TotalScore from '../TotalScore';
+
+export default {
+  title: 'Components/TotalScore',
+  component: TotalScore,
+};
+
+export const Player1 = {
+  args: {
+    player: 1,
+    points: 42,
+  },
+};
+
+export const Player2 = {
+  args: {
+    player: 2,
+    points: 84,
+  },
+};
---
+++ components/__stories__/TurnScore.stories.tsx
@@ -0,0 +1,20 @@
+import TurnScore from '../TurnScore';
+
+export default {
+  title: 'Components/TurnScore',
+  component: TurnScore,
+};
+
+export const Player1 = {
+  args: {
+    player: 1,
+    points: 42,
+  },
+};
+
+export const Player2 = {
+  args: {
+    player: 2,
+    points: 84,
+  },
+};
